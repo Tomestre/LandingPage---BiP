60d9d8dd3afa03d26010e22b4bd3fca2146822cd
Tomestre <gabrielveleci@gmail.com>
2023-05-01 18:36:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const iconNav = document.querySelector('.iconNav');
+
+iconNav.addEventListener('click', function() {
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('menu-active')
+});
