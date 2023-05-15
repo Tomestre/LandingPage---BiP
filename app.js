42f92ac4e95a86382735ccca68763fbef1278251
Tomestre <gabrielveleci@gmail.com>
2023-05-15 01:16:24 -0300

    windows version e mobile version

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const iconNav = document.querySelector('.iconNav');
+const menu = document.querySelector('.menu');
+const menuLink=document.querySelectorAll('nav a')
 
 iconNav.addEventListener('click', function() {
-  const menu = document.querySelector('.menu');
+  
   menu.classList.toggle('menu-active')
 });
+
+for(i=0; i < menuLink.length; i++) {
+menuLink[i].addEventListener('click',()=>{
+
+  menu.classList.remove('menu-active')
+})}
+
+
+
+const imgPeople = document.querySelectorAll('.container-peoples img')
+
+for(i=0; i < imgPeople.length; i++) {
+
+  const dataPeople = imgPeople[i].nextElementSibling
+  const imgPeopleSelected =imgPeople[i]
+
+  imgPeople[i].addEventListener('click',()=>{
+
+    imgPeopleSelected.classList.toggle('coluna2')
+
+    dataPeople.classList.toggle('data-active')
+  })}
+
